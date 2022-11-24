69aecb0fccc9c5c573c716996819de0952b126f6
eslemnuryildirim <118633017+eslemnuryildirim@users.noreply.github.com>
2022-11-24 17:34:12 +0300

    Add files via upload

---
+++ cos.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#define pi 3.141592	
+	int main(void)
+{
+
+	float a,b,c;
+	int alfa;
+{
+		 printf("\n İlk kenarı girin:"); scanf("%f",&a);
+ 		 printf("\n İkinci kenarı girin:"); scanf("%f",&b);
+ 		 printf("Açıyı girin\n"); scanf("%d",&alfa);
+{
+ 		 c=sqrt(pow(b,2)+pow(a,2)-2*b*a*cos(alfa);
+ 		 printf("3.kenar %f \n", c);
+}
+}
+return 0 ;
+}
